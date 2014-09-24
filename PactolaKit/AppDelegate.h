6f62e8ea1321cf182b6064330d66e053ac3bf4de
Thaddeus Ternes <thaddeus@bluetoo.co>
2014-09-23 23:09:43 -0500

    Initial Commit

---
+++ PactolaKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PactolaKit
+//
+//  Created by Thaddeus Ternes on 9/23/14.
+//  Copyright (c) 2014 Bluetoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PactolaKit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PactolaKit
+//
+//  Created by Thaddeus Ternes on 9/23/14.
+//  Copyright (c) 2014 Bluetoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
